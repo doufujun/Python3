ddea0223a74e8710c66ec7ec1cf1f696cfef29b9
doufujun <royyliu@126.com>
2019-04-25 17:16:59 +0800

    test

--- RegularExpression/RegularExpression2.py
+++
@@ -1,5 +0,0 @@
-# 泛匹配
-import re
-content = 'Hello 123 4567 World_This is a Regex Demo'
-result = re.match('^H.*o$', content)
-print(result)
--- RegularExpression/RegularExpression7.py
+++
@@ -1,7 +0,0 @@
-# 转义
-import re
-content = 'price is $5.00'
-# 使用\来转义特殊字符
-result = re.match('price is \$5\.00', content)
-print(result)
-print(result.group())
