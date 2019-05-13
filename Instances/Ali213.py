5278191e0784985280440a8772776ed48cd8217a
doufujun <royyliu@126.com>
2019-05-13 12:03:22 +0800

    V-1.7.0
    开始游侠热门游戏的抓取

--- Instances/Ali213.py
+++ Instances/Ali213.py
@@ -2,11 +2,12 @@ import requests
 
 def getOnePage(url) :
     response = requests.get(url)
-
+    return response
 
 def main () :
     url = 'http://www.ali213.net/zt/ztisitemap_hot.html'
-    print(getOnePage(url))
-
+    html = getOnePage(url)
+    print(html)
+    
 if __name__ == '__main__' :
     main()
